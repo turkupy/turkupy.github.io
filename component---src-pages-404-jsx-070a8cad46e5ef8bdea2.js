0cb6d2b3aa2fa4384133d5587d65a7811859873d
magda <stenius.mimmy@gmail.com>
2020-05-22 10:01:00 +0300

    Updates

---
+++ component---src-pages-404-jsx-070a8cad46e5ef8bdea2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{pssB:function(t,e,a){"use strict";a.r(e),a.d(e,"pageQuery",(function(){return o}));var n=a("q1tI"),u=a.n(n),r=a("L6Je"),s=a("zVja");e.default=function(t){var e=t.data.site.siteMetadata.title;return u.a.createElement(r.a,{title:e},u.a.createElement(s.a,{title:"404: Not Found"}),u.a.createElement("h1",null,"Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))};var o="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-070a8cad46e5ef8bdea2.js.map
