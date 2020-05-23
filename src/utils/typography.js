89ca2d0001bf5de1be6127a42a1ffc43ab860542
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2020-05-23 08:57:43 +0300

    Move styled components outside the component, remove unused code

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,12 +1,7 @@
 import Typography from "typography"
 import grandViewTheme from "typography-theme-grand-view"
-import { color } from "./styles"
 
-const typography = new Typography(grandViewTheme, {
-  overrideThemeStyles: () => ({
-    h2: { color: color.darkPink },
-  }),
-})
+const typography = new Typography(grandViewTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== "production") {
