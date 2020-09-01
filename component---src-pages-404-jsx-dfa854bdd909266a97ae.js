80f3bd224a9f36f2e7df2783a5df35f1491829f6
eevajonnapanula <eevajonna.panula@gmail.com>
2020-09-01 14:05:58 +0000

    Deploying to master from dev @ 528715817545d1e5ac0672bcadd0ca955b36886a 🚀

---
+++ component---src-pages-404-jsx-dfa854bdd909266a97ae.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{pssB:function(t,e,a){"use strict";a.r(e),a.d(e,"pageQuery",(function(){return o}));var n=a("q1tI"),u=a.n(n),r=a("L6Je"),s=a("zVja");e.default=function(t){var e=t.data.site.siteMetadata.title;return u.a.createElement(r.a,{title:e},u.a.createElement(s.a,{title:"404: Not Found"}),u.a.createElement("h1",null,"Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))};var o="3159585216"}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-dfa854bdd909266a97ae.js.map
