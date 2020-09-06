ec2bbf2c27da8fdc727f41b885eacfdac0c75eae
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2020-09-06 08:51:07 +0300

    Create new frontpage for every language

--- src/pages/fi/index.jsx
+++ src/pages/fi/index.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { graphql } from "gatsby"
 import PropTypes from "prop-types"
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
-import IndexPage from "../components/IndexPage"
+import Layout from "../../components/Layout"
+import SEO from "../../components/Seo"
+import IndexPage from "../../components/IndexPage"
 
 const Index = ({ data }) => {
-  const langCode = "en"
+  const langCode = "fi"
   const siteTitle = data.site.siteMetadata.title
   const events = data.events.edges
   // const organizers = data.organizers.edges
@@ -34,7 +34,7 @@ export const pageQuery = graphql`
     }
     events: allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { type: { eq: "event" }, lang: { eq: "en" } } }
+      filter: { frontmatter: { type: { eq: "event" }, lang: { eq: "fi" } } }
     ) {
       edges {
         node {
