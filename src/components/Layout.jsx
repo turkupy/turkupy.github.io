f5432e188a8869c164fe4f001d2a49fc60f36c4e
August <hachigatsu14@outlook.com>
2020-10-06 10:32:39 +0100

    added skip link and button menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 import logo from "../../content/assets/logo.png"
 import { createGlobalStyle } from "styled-components"
 import { colors } from "../utils/styles"
+import { getTranslation } from "../utils/translations/helpers"
 
 const GlobalStyles = createGlobalStyle`
   body { 
@@ -19,6 +20,41 @@ const GlobalStyles = createGlobalStyle`
  h2,h3 {
     color: ${colors.darkGrey};
     font-size: calc(1.25rem + 0.5vw);
+  }
+
+  button{
+    margin:2rem;
+    padding:1rem;
+    background:${colors.white};
+    border: dashed 6px ${colors.darkPink};
+  } 
+
+  button:hover{
+    cursor:pointer;
+    border:solid 6px ${colors.darkPink};
+    border-radius:5px;
+  }
+  
+  .skip-link{
+  color:  ${colors.white};
+  background: ${colors.darkPink};
+  padding:1rem;
+  position: absolute;
+  transform: translateY(-100%);
+  }
+
+  .skip-link:focus{
+    transform: translateY(0%);
+  }
+
+  .skip-link:hover{
+    cursor:pointer;
+    color:${colors.white};
+  }
+
+  nav{
+    display:flex;
+    justify-content:space-evenly;
   }
 `
 
@@ -37,12 +73,12 @@ const StyledLayout = styled.div`
     }
   }
 `
-
 const Footer = styled.footer`
   margin: 5rem 0 5rem 0;
 `
 
 const Layout = ({ children }) => {
+  const [langCode, setLangCode] = useState("en")
   return (
     <>
       <GlobalStyles />
@@ -54,8 +90,31 @@ const Layout = ({ children }) => {
               alt="Turku.py - programming community for women and nonbinaries"
             />
           </Link>
+          <a className="skip-link" href="#main">
+            {getTranslation(langCode, "skip-link")}
+          </a>
+          <nav>
+            <button
+              aria-label="Change content to English"
+              onClick={() => setLangCode("en")}
+            >
+              English
+            </button>
+            <button
+              aria-label="Change content to Finnish"
+              onClick={() => setLangCode("fi")}
+            >
+              Suomi
+            </button>
+            <button
+              aria-label="Change content to Swedish"
+              onClick={() => setLangCode("sv")}
+            >
+              Svenska
+            </button>
+          </nav>
         </header>
-        <main>{children}</main>
+        <main id="main">{children}</main>
         <Footer></Footer>
       </StyledLayout>
     </>
