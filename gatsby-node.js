291de63fbca4ea060456007acd5f6871b4bf2efa
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2020-10-11 08:14:24 +0300

    Generate pages in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,24 @@ const { createFilePath } = require("gatsby-source-filesystem")
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
+  // Create index pages
+
+  const langCodes = ["en", "fi", "sv"]
+  const indexPage = path.resolve("./src/templates/IndexPage.jsx")
+
+  langCodes.forEach(code => {
+    const path = code == "en" ? "/" : `/${code}`
+
+    createPage({
+      path,
+      component: indexPage,
+      context: {
+        langCode: code,
+      },
+    })
+  })
+
+  // Create event detail pages
   const eventPage = path.resolve("./src/templates/EventPage.jsx")
   const result = await graphql(
     `
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,3 +2,12 @@ export const formatDate = dateString => {
   const date = new Date(dateString)
   return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
 }
+
+export const getLangCode = slug => {
+  const regex = /(\bfi)|(\ben)|(\bsv)/g
+  const regexedLangCode = slug.match(regex)
+  // Default to en if lang code is not found
+  const langCode = regexedLangCode ? regexedLangCode[0] : "en"
+
+  return langCode
+}
