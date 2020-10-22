b00f7346e70cb2441030d2be79d188a028e08bda
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2020-10-22 11:29:49 +0300

    Add langcode to layout

--- src/templates/EventsPage.jsx
+++ src/templates/EventsPage.jsx
@@ -4,14 +4,17 @@ import PropTypes from "prop-types"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Events from "../components/Events"
+import { getLangCode } from "../utils/utils"
 
-const EventsPage = ({ data }) => {
+const EventsPage = ({ data, location }) => {
+  const { pathname } = location
+  const langCode = getLangCode(pathname)
   const siteTitle = data.site.siteMetadata.title
   const pastEvents = data.pastEvents.edges
   const upcomingEvents = data.upcomingEvents.edges
 
   return (
-    <Layout title={siteTitle}>
+    <Layout title={siteTitle} langCode={langCode}>
       <SEO title={siteTitle} />
       <h2>Upcoming Events</h2>
       <Events events={upcomingEvents} />
@@ -23,6 +26,7 @@ const EventsPage = ({ data }) => {
 
 EventsPage.propTypes = {
   data: PropTypes.any,
+  location: PropTypes.any,
 }
 
 export default EventsPage
